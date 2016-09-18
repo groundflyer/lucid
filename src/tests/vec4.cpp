abd3929d986c7676728cf90a689a08453e0d47de
Roman Saldygashev <sldg.roman@gmail.com>
2016-09-18 19:39:10 +0500

    refactoring

--- src/tests/vec4.cpp
+++ src/tests/vec4.cpp
@@ -11,19 +11,22 @@ using Vec4u = Vector<unsigned, 4>;
 
 int main()
 {
-    Vec4 a(1., -3., 2., 4.), b(-1., 3., -2., 4);
-    auto test = a + b;
-    test *= b;
-    auto c = (test / 4);
-    test *= c;
-    Vec4 init_list_test = Vec4{1,2,3,4,7,8,9};
-    TEST_AND_COUT(init_list_test);
-    TEST_AND_COUT(sizeof(test));
-    TEST_AND_COUT(a);
-    TEST_AND_COUT(b);
-    TEST_AND_COUT((a == b));
-    TEST_AND_COUT((a ^ b));
-    TEST_AND_COUT((a % b));
+    TEST_AND_COUT((Vec4{1,2,3,4,7,8,9}));
+    TEST_AND_COUT(sizeof((Vec4{})));
+    TEST_AND_COUT(Vec4(1., -3., 2., 4.));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) == Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) * Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) + Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) - Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) / Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) ^ Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT((Vec4(1., -3., 2., 4.) % Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT(length(Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT(normalize(Vec4(-1., 3., -2., 4)));
+    TEST_AND_COUT(sum(Vec4(1,1,1,1)));
+    TEST_AND_COUT(avg(Vec4(1,2,3,4)));
+    TEST_AND_COUT(max(Vec4(5,-2,4,1)));
+    TEST_AND_COUT(min(Vec4(5,-2,4,1)));
     TEST_AND_COUT(Vec4((Vec3(1.,2.,3).cbegin())));
     TEST_AND_COUT(Vec3((Vec4(1.,2.,3.,4).cbegin())));
     TEST_AND_COUT(Vec4u(Vec4(1.5,2.8,3.6,4.1)));
