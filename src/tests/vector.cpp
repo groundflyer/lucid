72bdaef9ca7e99fccf5d3ec50c39bf66e82fe545
Roman Saldygashev <sldg.roman@gmail.com>
2018-11-27 20:46:13 +0300

    add multi value constructor test

--- src/tests/vector.cpp
+++ src/tests/vector.cpp
@@ -32,56 +32,43 @@ get_dist() noexcept
     return dist;
 }
 
-template <typename T>
-struct typeinfo;
+#define MAKE_TYPEINFO_STRING(TYPE)                    \
+    constexpr auto get_typeinfo_string(TYPE) noexcept \
+    { return #TYPE; }
 
-template <>
-struct typeinfo<int>
-{ static const constexpr char string[] = "int"; };
+MAKE_TYPEINFO_STRING(int)
+MAKE_TYPEINFO_STRING(bool)
+MAKE_TYPEINFO_STRING(char)
+MAKE_TYPEINFO_STRING(long)
+MAKE_TYPEINFO_STRING(float)
+MAKE_TYPEINFO_STRING(double)
+MAKE_TYPEINFO_STRING(size_t)
+MAKE_TYPEINFO_STRING(long double)
+MAKE_TYPEINFO_STRING(unsigned int)
+MAKE_TYPEINFO_STRING(unsigned char)
+MAKE_TYPEINFO_STRING(unsigned short)
 
-template <>
-struct typeinfo<bool>
-{ static const constexpr char string[] = "bool"; };
+template <typename... Ts>
+constexpr auto get_typeinfo_string(std::tuple<Ts...>) noexcept
+{ return "{}"_format(std::tuple(get_typeinfo_string(Ts{})...)); }
 
-template <>
-struct typeinfo<char>
-{ static const constexpr char string[] = "char"; };
+template <size_t TupleSize, size_t Start,
+          typename RandomEngine, typename Typelist, size_t... I>
+auto _random_cycled_tuple(RandomEngine& g, Typelist, std::index_sequence<I...>)
+{ return std::tuple(get_dist<typename typelist_element<(Start + I) % TupleSize, Typelist>::type>()(g)...); }
 
-template <>
-struct typeinfo<long>
-{ static const constexpr char string[] = "long"; };
-
-template <>
-struct typeinfo<float>
-{ static const constexpr char string[] = "float"; };
-
-template <>
-struct typeinfo<double>
-{ static const constexpr char string[] = "double"; };
-
-template <>
-struct typeinfo<size_t>
-{ static const constexpr char string[] = "size_t"; };
-
-template <>
-struct typeinfo<long double>
-{ static const constexpr char string[] = "long double"; };
-
-template <>
-struct typeinfo<unsigned int>
-{ static const constexpr char string[] = "unsigned int"; };
-
-template <>
-struct typeinfo<unsigned char>
-{ static const constexpr char string[] = "unsigned char"; };
+template <size_t TupleSize, size_t Start,
+          typename RandomEngine, typename... Ts>
+auto random_cycled_tuple(RandomEngine& g, typelist<Ts...> tl)
+{ return _random_cycled_tuple<sizeof...(Ts), Start>(g, tl, std::make_index_sequence<TupleSize>{}); }
 
 template <typename Vec, typename T, typename RandomEngine>
 auto
 _single_value_constructor_test(std::string_view type_string,
                                RandomEngine& g,
-                               size_t num_tests) noexcept
+                               const size_t num_tests) noexcept
 {
-    return test_property("{}({})"_format(type_string, typeinfo<T>::string),
+    return test_property("{}({})"_format(type_string, get_typeinfo_string(T{})),
                          [](const auto feed) { return Vec(feed); },
                          [](const auto property, const auto feed) { return any(property != feed); },
                          [&](){ return get_dist<T>()(g); },
@@ -92,25 +79,72 @@ template <typename Vec, typename RandomEngine, typename... Ts>
 auto
 single_value_constructor_test(std::string_view type_string,
                               RandomEngine& g,
-                              size_t num_tests,
+                              const size_t num_tests,
                               typelist<Ts...>) noexcept
 { return (0 + ... + _single_value_constructor_test<Vec, Ts>(type_string, g, num_tests)); }
 
-using test_types = typelist<int, bool, char, long, float, double, size_t, long double, unsigned, unsigned char>;
-static const constexpr size_t MaxN = 4;
+template <size_t I, typename A, typename B>
+bool
+not_equal(const A& a, const B& b)
+{ return std::get<I>(a) != static_cast<std::decay_t<decltype(std::get<I>(a))>>(std::get<I>(b)); }
+
+template <typename A, typename B, size_t... I>
+bool
+assert_tuples(const A& a, const B& b, std::index_sequence<I...>)
+{
+    static_assert(std::tuple_size_v<A> == std::tuple_size_v<B>, "Tuples must have the same size");
+
+    return (false || ... || not_equal<I>(a, b));
+}
+
+template <typename Vec, size_t N, size_t Shift, typename RandomEngine, typename GenTypes>
+auto
+__multi_value_constructor_test(std::string_view type_string,
+                              RandomEngine& g,
+                              const size_t num_tests,
+                              GenTypes tl)
+{
+    auto tg = [&](){ return random_cycled_tuple<N, Shift>(g, tl); };
+    return test_property("{}{}"_format(type_string, get_typeinfo_string(tg())),
+                         [](const auto& feed){ return construct<Vec>(feed); },
+                         [](const Vec& property, const auto& feed) { return assert_tuples(property, feed, std::make_index_sequence<N>{}); },
+                         tg,
+                         num_tests);
+}
+
+template <typename Vec, size_t N, typename RandomEngine, typename GenTypes, size_t... I>
+auto
+_multi_value_constructor_test(std::string_view type_string,
+                             RandomEngine& g,
+                             const size_t num_tests,
+                             GenTypes tl,
+                             std::index_sequence<I...>)
+{ return (0u + ... + __multi_value_constructor_test<Vec, N, I>(type_string, g, num_tests, tl)); }
+
+template <typename Vec, size_t N, typename RandomEngine, typename... GenTypes>
+auto
+multi_value_constructor_test(std::string_view type_string,
+                             RandomEngine& g,
+                             const size_t num_tests,
+                             typelist<GenTypes...> gen_types)
+{ return _multi_value_constructor_test<Vec, N>(type_string, g, num_tests, gen_types, std::make_index_sequence<sizeof...(GenTypes) - N + 1>{}); }
+
+using test_types = typelist<int, bool, char, long, float, double, size_t, long double, unsigned, unsigned char, unsigned short>;
 
 template <typename T, size_t N, typename RandomEngine>
 auto
-vector_test(RandomEngine& g, size_t num_tests) noexcept
+vector_test(RandomEngine& g, const size_t num_tests) noexcept
 {
     using Vec = Vector<T, N>;
-    const auto type_string = "Vector<{}, {}>"_format(typeinfo<T>::string, N);
+    const auto type_string = "Vector<{}, {}>"_format(get_typeinfo_string(T{}), N);
 
     unsigned ret = 0;
 
-    ret += single_value_constructor_test<Vec>(type_string, g, num_tests, test_types{});
+    auto tts = test_types{};
 
-    ret += test_property("{}(std::array<{}, {}>)"_format(type_string, typeinfo<T>::string, N),
+    ret += single_value_constructor_test<Vec>(type_string, g, num_tests, tts);
+
+    ret += test_property("{}(std::array<{}, {}>)"_format(type_string, get_typeinfo_string(T{}), N),
                          [](const std::array<T, N>& feed) { return Vec(feed); },
                          [](const Vec& property, const std::array<T, N>& feed)
                          {
@@ -123,8 +157,12 @@ vector_test(RandomEngine& g, size_t num_tests) noexcept
                          { return get_dist<T>().template operator()<N>(g); },
                          num_tests);
 
+    ret += multi_value_constructor_test<Vec, N>(type_string, g, num_tests, tts);
+
     return ret;
 }
+
+static const constexpr size_t MaxN = 4;
 
 template <typename T, typename RandomEngine, size_t... Ns>
 auto vector_test_n(RandomEngine& g, size_t num_tests, std::index_sequence<Ns...>) noexcept
@@ -136,7 +174,7 @@ auto vector_test_t(RandomEngine& g, size_t num_tests, typelist<Ts...>) noexcept
 
 int main(int argc, char* argv[])
 {
-    const size_t num_tests = argc == 2 ? std::stoll(argv[1]) : 10000;
+    const size_t num_tests = argc == 2 ? std::stoll(argv[1]) : 1000000;
 
     int ret = 0;
 
@@ -147,125 +185,120 @@ int main(int argc, char* argv[])
     RandomDistribution unsigned_dist(0u, 10000u);
     RandomDistribution double_dist(-1e+10, 1e+10);
 
-    const static constexpr size_t N = 4;
-
     init_log();
 
     ret += vector_test_t(g, num_tests, test_types{});
 
-    ret += test_property("Vector<float, N>(float, float, float, float)",
-                         [](auto&& feed) { return construct<Vector<float, 4>>(forward<decltype(feed)>(feed)); },
-                         [](auto&& property, auto&& feed)
-                         {
-                             return sum(property) != apply([](auto... vals) { return (0 + ... + vals); },
-                                                           forward<decltype(feed)>(feed));
-                         },
-                         [&]() { return tuple(float_dist(g), float_dist(g), float_dist(g), float_dist(g)); },
-                         num_tests);
-
-    ret += test_property("Vector<double, N>(double, float, int, unsigned)",
-                         [](auto&& feed) { return construct<Vector<double, 4>>(forward<decltype(feed)>(feed)); },
-                         [](auto&& property, auto&& feed)
-                         {
-                             return sum(property) != apply([](auto... vals) { return (0 + ... + vals); },
-                                                           forward<decltype(feed)>(feed));
-                         },
-                         [&]() { return tuple(double_dist(g), float_dist(g), int_dist(g), unsigned_dist(g)); },
-                         num_tests);
-
-    // ret += test_property("any(Vector<bool, N>)",
-    //                      [](const Vector<bool, N> feed) { return any(feed); },
-    //                      [](const bool property, const Vector<bool, N> feed)
+    // ret += test_property("Vector<float, N>(float, float, float, float)",
+    //                      [](auto&& feed) { return construct<Vector<float, 4>>(forward<decltype(feed)>(feed)); },
+    //                      [](auto&& property, auto&& feed)
     //                      {
-    //                          return property != apply([](auto... vals) { return (true && ... && vals); },
-    //                                                   feed);
+    //                          return sum(property) != apply([](auto... vals) { return (0 + ... + vals); },
+    //                                                        forward<decltype(feed)>(feed));
     //                      },
-    //                      [&]() { return Vector(bool_dist.operator()<N>(g)); },
+    //                      [&]() { return tuple(float_dist(g), float_dist(g), float_dist(g), float_dist(g)); },
     //                      num_tests);
 
-    ret += test_property("Vector<float, N> +- float",
-                         [](const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return vec + val;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return any(property - val != vec);
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
-                         num_tests);
+    // ret += test_property("Vector<double, N>(double, float, int, unsigned)",
+    //                      [](auto&& feed) { return construct<Vector<double, 4>>(forward<decltype(feed)>(feed)); },
+    //                      [](auto&& property, auto&& feed)
+    //                      {
+    //                          return sum(property) != apply([](auto... vals) { return (0 + ... + vals); },
+    //                                                        forward<decltype(feed)>(feed));
+    //                      },
+    //                      [&]() { return tuple(double_dist(g), float_dist(g), int_dist(g), unsigned_dist(g)); },
+    //                      num_tests);
 
-    ret += test_property("Vector<float, N> +- int",
-                         [](const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return vec + val;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return any(property - val != vec);
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), int_dist(g)); },
-                         num_tests);
+    // // ret += test_property("any(Vector<bool, N>)",
+    // //                      [](const Vector<bool, N>& feed) { return any(feed); },
+    // //                      [](const bool property, const Vector<bool, N>& feed)
+    // //                      { return property != apply([](auto... vals) { return (true && ... && vals); }, feed); },
+    // //                      [&]() { return Vector(get_dist<bool>().template operator()<N>(g)); },
+    // //                      num_tests);
 
-    ret += test_property("Vector<float, N> +- Vector<float, N>",
-                         [](const auto feed)
-                         {
-                             const auto& [vec1, vec2] = feed;
-                             return vec1 + vec2;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec1, vec2] = feed;
-                             return any(property - vec1 != vec2);
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), Vector(float_dist.operator()<N>(g))); },
-                         num_tests);
+    // ret += test_property("Vector<float, N> +- float",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return vec + val;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return any(property - val != vec);
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
+    //                      num_tests);
 
-    ret += test_property("Vector<float, N> */ int",
-                         [](const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return vec * val;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return any(!almost_equal(property / val, vec));
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), int_dist(g)); },
-                         num_tests);
+    // ret += test_property("Vector<float, N> +- int",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return vec + val;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return any(property - val != vec);
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), int_dist(g)); },
+    //                      num_tests);
 
-    ret += test_property("Vector<float, N> */ float",
-                         [](const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return vec * val;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec, val] = feed;
-                             return any(!almost_equal(property / val, vec));
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
-                         num_tests);
+    // ret += test_property("Vector<float, N> +- Vector<float, N>",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec1, vec2] = feed;
+    //                          return vec1 + vec2;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec1, vec2] = feed;
+    //                          return any(property - vec1 != vec2);
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), Vector(float_dist.operator()<N>(g))); },
+    //                      num_tests);
 
-    ret += test_property("Vector<float, N> */ Vector<float, N>",
-                         [](const auto feed)
-                         {
-                             const auto& [vec1, vec2] = feed;
-                             return vec1 * vec2;
-                         },
-                         [](const auto property, const auto feed)
-                         {
-                             const auto& [vec1, vec2] = feed;
-                             return any(!almost_equal(property / vec2, vec1));
-                         },
-                         [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
-                         num_tests);
+    // ret += test_property("Vector<float, N> */ int",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return vec * val;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return any(!almost_equal(property / val, vec));
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), int_dist(g)); },
+    //                      num_tests);
+
+    // ret += test_property("Vector<float, N> */ float",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return vec * val;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec, val] = feed;
+    //                          return any(!almost_equal(property / val, vec));
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
+    //                      num_tests);
+
+    // ret += test_property("Vector<float, N> */ Vector<float, N>",
+    //                      [](const auto feed)
+    //                      {
+    //                          const auto& [vec1, vec2] = feed;
+    //                          return vec1 * vec2;
+    //                      },
+    //                      [](const auto property, const auto feed)
+    //                      {
+    //                          const auto& [vec1, vec2] = feed;
+    //                          return any(!almost_equal(property / vec2, vec1));
+    //                      },
+    //                      [&]() { return pair(Vector(float_dist.operator()<N>(g)), float_dist(g)); },
+    //                      num_tests);
 
     return ret;
 }
