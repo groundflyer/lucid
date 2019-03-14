fa60a77c75f4e995f2ef2dc4cfa26c075ea311bf
Roman Saldygashev <sldg.roman@gmail.com>
2019-03-14 22:14:01 +0000

    add disk sampling

--- src/traversal/disk.hpp
+++ src/traversal/disk.hpp
@@ -13,7 +13,7 @@ namespace yapt
     struct Disk_
     {
         Plane_<Container> plane;
-        real radius2;
+        real radius;
 
         constexpr
         Disk_() {}
@@ -25,7 +25,7 @@ namespace yapt
               const Normal_<Container2>& _normal,
               const real _radius) :
             plane(_position, _normal),
-            radius2(math::pow<2>(_radius))
+            radius(_radius)
         {}
     };
 
@@ -44,9 +44,10 @@ namespace yapt
 	intersect(const Ray_<RayContainer>& ray,
               const Disk_<DiskContainer>& prim) noexcept
     {
-        auto plane_isect = intersect(ray, prim.plane);
+        const auto& plane = prim.plane;
+        auto plane_isect = intersect(ray, plane);
         const auto& [o, d] = ray;
-        plane_isect.intersect &= length2((o + d * plane_isect.t) - prim.plane.position) <= prim.radius2;
+        plane_isect.intersect &= length((o + d * plane_isect.t) - plane.position) <= prim.radius;
         return plane_isect;
     }
 
@@ -54,8 +55,22 @@ namespace yapt
 			  template <typename, size_t> typename RayContainer,
               template <typename, size_t> typename IsectContainer>
     constexpr auto
-    compute_normal(const Ray_<RayContainer>& ray,
-                   const Intersection_<IsectContainer>& isect,
-                   const Disk_<DiskContainer>& prim) noexcept
-    { return compute_normal(ray, isect, static_cast<Plane_<DiskContainer>>(prim)); }
+    normal(const Ray_<RayContainer>& ray,
+           const Intersection_<IsectContainer>& isect,
+           const Disk_<DiskContainer>& prim) noexcept
+    { return normal(ray, isect, prim.plane); }
+
+    template <template <typename, size_t> typename Container,
+              typename Generator>
+    constexpr auto
+    sample(Generator&& gen,
+           const Disk_<Container>& prim) noexcept
+    {
+        const auto& [pos, zaxis] = prim.plane;
+        const auto sr = prim.radius * math::sqrt(gen());
+        const auto theta = 2_r * math::PI<real> * gen();
+        const Vec3 point{sr * math::sin(theta), sr * math::cos(theta), 0_r};
+        const auto [xaxis, yaxis] = basis(zaxis);
+        return transpose(Mat3(xaxis, yaxis, zaxis)).dot(point) + pos;
+    }
 }
