023e040ef37ce9f6f5139dbbbcff7639d6013a4b
Roman Saldygashev <sldg.roman@gmail.com>
2019-12-30 23:13:46 +0500

    update viewport test

--- src/tests/viewport.cpp
+++ src/tests/viewport.cpp
@@ -1,6 +1,7 @@
 // -*- C++ -*-
 // viewport.cpp
 #include <gui/viewport.hpp>
+#include <image_reconstruction/film.hpp>
 #include <image_reconstruction/filtering.hpp>
 #include <integrators/basic.hpp>
 #include <sampling/film.hpp>
@@ -16,48 +17,38 @@ int main(int argc, char* argv[])
 {
     const real radius{argc > 1 ? std::stof(argv[1]) : 2};
 
-    vp::init(640, 640);
+    Viewport::init(640, 640);
 
-    ScanlineImage<float, 4> img(vp::res);
+    Film<ScanlineImage<float, 4>> film(Viewport::get_res());
 
-    const Vec2 res{vp::res};
-    const Vec2 pixel_size = Vec2(1_r) / res;
-    const real filter_rad = pixel_radius(pixel_size) * radius;
+    const Vec2 res{Viewport::get_res()};
+    const real filter_rad = film.pixel_radius() * radius;
     const PixelUpdate updater{TriangleFilter(filter_rad)};
 
-    // const auto& [w, h]       = vp::res;
-    // const real         ratio = static_cast<real>(w) / static_cast<real>(h);
-    // perspective::shoot cam   = make_cam(ratio);
-    // const auto         room_geo   = make_room();
-    // const auto         mat_getter = get_mat_getter();
-    // using Simple = Constant<std::decay_t<decltype(room_geo)>, std::decay_t<decltype(mat_getter)>>;
-    // for(auto it = img.begin(); it != img.end(); ++it)
-    // {
-    //     const Vec2 pp = device_coords(it.pos(), res, pixel_size);
-    //     // const Vec2 sample_pos = sample_pixel(g, pixel_size, pp);
-    //     Simple ig{cam(pp), &room_geo, &mat_getter, pp};
-    //     const auto result = ig();
-    //     *it = RGBA(result.second);
-    //     // update_pixels(img, updater, res, pixel_size, result);
-    // }
-
-    const Sample samples[]{Sample{Vec2(-0.25_r), RGB(0.2_r, 1_r, 0.1_r)},
-                           Sample{Vec2(0.25_r), RGB(1_r, 0.2_r, 0.1_r)},
-                           Sample{Vec2(-0.25_r, 0.25_r), RGB(0.1_r, 0.9_r, 1_r)},
-                           Sample{Vec2(0.25_r, -0.25_r), RGB(1_r, 0.9_r, 0.1_r)}};
-
-    for (const Sample& sample : samples)
-        update_pixels(img, updater, res, pixel_size, sample);
-
-    vp::load_img(img);
-
-    while(vp::active())
+    const auto& [w, h]       = Viewport::get_res();
+    const real         ratio = static_cast<real>(w) / static_cast<real>(h);
+    const perspective::shoot cam   = CornellBox::camera(ratio);
+    const auto         room_geo   = CornellBox::geometry();
+    const auto         mat_getter = CornellBox::mat_getter();
+    using Simple = Constant<std::decay_t<decltype(room_geo)>, std::decay_t<decltype(mat_getter)>>;
+    for(auto it = film.img.begin(); it != film.img.end(); ++it)
     {
-        vp::draw();
+        const Vec2 pp = film.device_coords(it.pos());
+        const Vec2 sample_pos = sample_pixel(g, film.pixel_size, pp);
+        Simple ig{cam(sample_pos), &room_geo, &mat_getter, sample_pos};
+        const auto result = ig();
+        update_pixels(film, updater, result);
+    }
+
+    Viewport::load_img(film.img);
+
+    while(Viewport::active())
+    {
+        Viewport::draw();
         glfwPollEvents();
     }
 
-    vp::cleanup();
+    Viewport::cleanup();
 
     return 0;
 }
