641bf39f83860774e0a2cddb9a250ea4ade5075b
Roman Saldygashev <sldg.roman@gmail.com>
2020-03-31 11:05:43 +0500

    remove useless exception

--- src/utils/range.hpp
+++ src/utils/range.hpp
@@ -4,24 +4,18 @@
 
 #pragma once
 
-#include "exceptions.hpp"
-
+#include <algorithm>
 #include <limits>
 #include <type_traits>
 
-
 namespace lucid
 {
-    template <typename T>
-    constexpr auto
-    range(const T& lower, const T& upper)
-    {
-		static_assert(std::is_arithmetic_v<T>, "T is not arithmetic!");
-
-        if(lower >= upper)
-            throw incorrect_range<T>{lower, upper};
-
-        return [=](const T& val) constexpr
-               { return val >= lower && val <= upper; };
-    }
+template <typename T>
+constexpr auto
+range(T lower, T upper) noexcept
+{
+    static_assert(std::is_arithmetic_v<T>, "T is not arithmetic!");
+    std::tie(lower, upper) = std::minmax(lower, upper);
+    return [=](const T& val) constexpr { return val >= lower && val <= upper; };
 }
+} // namespace lucid
