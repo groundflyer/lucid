8576f5bf444cbed1c9446845cc5c17dbeb20bc50
Roman Saldygashev <sldg.roman@gmail.com>
2020-09-18 21:03:00 +0500

    log benchmark results to file

--- src/benchmarks/benchmark.hpp
+++ src/benchmarks/benchmark.hpp
@@ -13,11 +13,19 @@
 
 #include <vector>
 
+#include <cstdio>
+
+#ifdef REPO_HASH
+const constexpr char repo_hash[] = REPO_HASH;
+#else
+const constexpr char repo_hash[] = "nonrepo";
+#endif
+
 namespace lucid
 {
 template <typename G, typename R>
 void
-bench(std::size_t n, std::string_view name, G&& g, R&& r) noexcept
+bench(const std::size_t n, std::string_view name, G&& g, R&& r, std::FILE* log = nullptr) noexcept
 {
     auto vals = ranges::views::generate_n(g, n) | ranges::to<std::vector>();
 
@@ -27,10 +35,14 @@ bench(std::size_t n, std::string_view name, G&& g, R&& r) noexcept
 
     const auto elapsed = timer.elapsed();
 
-    fmt::print("{}: {} for {} runs; avg: {}\n",
-               name,
-               std::chrono::duration_cast<std::chrono::milliseconds>(elapsed),
-               n,
-               elapsed / n);
+    const auto ms  = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
+    const auto avg = elapsed / n;
+    fmt::print("{}: {} for {} runs; avg: {}\n", name, ms, n, avg);
+
+    if(log)
+    {
+        std::time_t t = std::time(nullptr);
+        fmt::print(log, "{} {:%Y%m%d} {}\n", repo_hash, fmt::localtime(t), avg);
+    }
 }
 } // namespace lucid
