7592d254ec900fe0302babf13b71bb303ebdec8f
Roman Saldygashev <sldg.roman@gmail.com>
2020-09-22 13:15:26 +0500

    use -stdlib only for cxx compiler

--- cmake/Clang.cmake
+++ cmake/Clang.cmake
@@ -5,7 +5,7 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND USE_LIBCXX)
   check_cxx_compiler_flag("-stdlib=libc++ -lc++abi" LibCxx)
 
   if(LibCxx)
-    add_compile_options(-stdlib=libc++)
+    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
     add_link_options(-stdlib=libc++ -lc++abi)
     message(STATUS "Using LLVM libc++")
   endif()
