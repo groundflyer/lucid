0c3a8fb7d930b2c6ca38e2bfe3fc4c577a57ebb1
Roman Saldygashev <sldg.roman@gmail.com>
2020-10-22 16:54:57 +0500

    document property-based testing

--- src/tests/property_test.hpp
+++ src/tests/property_test.hpp
@@ -2,26 +2,60 @@
 // property_test.hpp
 //
 
+/// @file
+/// Simple property-based testing.
+
 #pragma once
 
-#include <base/vector.hpp>
-
-#include <fmt/ranges.h>
+#include <fmt/format.h>
 
 #include <string_view>
 #include <utility>
 
+/// @brief Test a @p property of @p testing function.
+/// @param n Number of runs.
+/// @param threshold Ratio of runs allowed to fail.
+/// @param property_name Label to display.
+/// @param generator Callable that generates a unique value each time its called.
+/// @param testing Function that takes the output of @p generator and returns some value.
+/// @param property Function that takes the outputs of @p generator and @p testing and asserts them
+/// returning bool.
+/// @returns true if test failed otherwise false.
+///
+/// Simplified approach to property-based testing.
+///
+/// Unlike classic unit testing, in property-based testing
+/// we test functions many times against various values.
+/// Properties are invariant, so it makes testing more robust.
+///
+/// First, we need to find a property of the testing function,
+/// or we can just use inverse functions, e.g. minus and plus thus
+/// testing two functions at the time.
+///
+/// Then we define our @p generator.
+/// It outputs random values in a valid range
+/// acceptable by @p testing function.
+///
+/// @p testing is the function we'd like to test.
+///
+/// @p property checks if the property is preserved.
+/// It accepts the output of @p testing as the first argument
+/// and the output of @p generator as the second argument.
+/// In case of fail it returns true, otherwise false.
+///
+/// The test is probablistic: if the portion of failed tests
+/// is less than @p threshold, test passed.
 template <typename Testing, typename Property, typename Generator>
 bool
-test_property(const size_t     n,
-              const double     threshold,
-              std::string_view property_name,
-              Generator&&      generator,
-              Testing&&        testing,
-              Property&&       property)
+test_property(const std::size_t n,
+              const double      threshold,
+              std::string_view  property_name,
+              Generator&&       generator,
+              Testing&&         testing,
+              Property&&        property)
 {
-    size_t sum = 0;
-    for(size_t i = 0; i < n; ++i)
+    std::size_t sum = 0;
+    for(std::size_t i = 0; i < n; ++i)
     {
         auto&& feed    = generator();
         using FeedType = std::decay_t<decltype(feed)>;
@@ -30,9 +64,9 @@ test_property(const size_t     n,
         sum += static_cast<size_t>(result);
     }
 
-    const auto error = static_cast<double>(sum) / static_cast<double>(n);
+    const double error = static_cast<double>(sum) / static_cast<double>(n);
 
-    const auto                  ret     = error > threshold;
+    const bool                  ret     = error > threshold;
     static const constexpr char RED[]   = "\033[31m";
     static const constexpr char GREEN[] = "\033[32m";
     static const constexpr char RESET[] = "\033[0m";
@@ -43,15 +77,3 @@ test_property(const size_t     n,
 
     return ret > 0;
 }
-
-template <size_t I, typename A, typename B>
-constexpr bool
-not_equal(A&& a, B&& b) noexcept
-{
-    return std::get<I>(std::forward<A>(a)) != std::get<I>(std::forward<B>(b));
-}
-
-template <typename A, typename B, size_t... I>
-constexpr bool
-assert_tuples(A&& a, B&& b, std::index_sequence<I...>) noexcept
-{ return (false || ... || not_equal<I>(std::forward<A>(a), std::forward<B>(b))); }
