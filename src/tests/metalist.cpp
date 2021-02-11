e5195a0a650053ba8b07ee7539d7edb9cef4dbbf
Roman Saldygashev <sldg.roman@gmail.com>
2021-02-11 21:06:08 +0700

    added del meta function

--- src/tests/metalist.cpp
+++ src/tests/metalist.cpp
@@ -8,12 +8,12 @@ using namespace lucid;
 
 static_assert(std::is_same_v<typelist<int, int, int, int>, typename repeat_type<4, int>::type>);
 
-static_assert(std::is_same_v<std::integer_sequence<int, -10, 15, 20, -900>,
-                             typename join<std::integer_sequence<int, -10, 15>,
-                                           std::integer_sequence<int, 20, -900>>::type>);
+static_assert(std::is_same_v<
+              std::integer_sequence<int, -10, 15, 20, -900>,
+              join_t<std::integer_sequence<int, -10, 15>, std::integer_sequence<int, 20, -900>>>);
 
-static_assert(std::is_same_v<std::integer_sequence<int, 10, 10, 10, 10, 10>,
-                             typename repeat_integer<5, int, 10>::type>);
+static_assert(
+    std::is_same_v<std::integer_sequence<int, 10, 10, 10, 10, 10>, repeat_integers_t<5, int, 10>>);
 
 static_assert(std::is_same_v<typename typelist<bool, int, float, double>::template at<2>, float>);
 
@@ -21,11 +21,15 @@ static_assert(typelist<bool, int, float, double>::template index<float>() == 2);
 
 using rev_seq1 = std::integer_sequence<int, 1, 2, 3, 4>;
 using rev_seq2 = std::integer_sequence<int, 4, 3, 2, 1>;
-static_assert(std::is_same_v<typename reverse<rev_seq1>::type, rev_seq2>);
+static_assert(std::is_same_v<reverse_t<rev_seq1>, rev_seq2>);
 
 using fill_seq1 = std::integer_sequence<int, 1, 2, 3, 4>;
 using fill_seq2 = std::integer_sequence<int, 1, 2, 3, 4, 0, 0, 0>;
-static_assert(std::is_same_v<typename fill_val<3, int, 0, fill_seq1>::type, fill_seq2>);
+static_assert(std::is_same_v<fill_val_t<fill_seq1, 3, 0>, fill_seq2>);
+
+using del_seq1 = std::integer_sequence<int, 1, 2, 3, 4>;
+using del_seq2 = std::integer_sequence<int, 1, 2, 4>;
+static_assert(std::is_same_v<del_t<del_seq1, 3>, del_seq2>);
 
 int
 main()
--- src/utils/intlist.hpp
+++ src/utils/intlist.hpp
@@ -10,6 +10,18 @@
 
 namespace lucid
 {
+template <auto Val, typename List>
+struct cons_int;
+
+template <typename Int, Int Head, Int... Rest>
+struct cons_int<Head, std::integer_sequence<Int, Rest...>>
+{
+    using type = std::integer_sequence<Int, Head, Rest...>;
+};
+
+template <auto Val, typename List>
+using cons_int_t = typename cons_int<Val, List>::type;
+
 template <typename List1, typename List2>
 struct join;
 
@@ -19,21 +31,27 @@ struct join<std::integer_sequence<Int, Ints1...>, std::integer_sequence<Int, Int
     using type = std::integer_sequence<Int, Ints1..., Ints2...>;
 };
 
+template <typename List1, typename List2>
+using join_t = typename join<List1, List2>::type;
+
 template <std::size_t N, typename Int, Int... Vals>
-struct repeat_integer;
+struct repeat_integers;
 
 template <typename Int, Int... Vals>
-struct repeat_integer<1, Int, Vals...>
+struct repeat_integers<1, Int, Vals...>
 {
     using type = std::integer_sequence<Int, Vals...>;
 };
 
 template <std::size_t N, typename Int, Int... Vals>
-struct repeat_integer
+struct repeat_integers
 {
-    using type = typename join<std::integer_sequence<Int, Vals...>,
-                               typename repeat_integer<(N - 1), Int, Vals...>::type>::type;
+    using type = join_t<std::integer_sequence<Int, Vals...>,
+                               typename repeat_integers<(N - 1), Int, Vals...>::type>;
 };
+
+template <std::size_t N, typename Int, Int... Vals>
+using repeat_integers_t = typename repeat_integers<N, Int, Vals...>::type;
 
 template <std::size_t I, typename List>
 struct elem;
@@ -52,6 +70,9 @@ struct elem<I, std::integer_sequence<Int, Head, Vals...>>
     static constexpr Int value = elem<I - 1, std::integer_sequence<Int, Vals...>>::value;
 };
 
+template <std::size_t I, typename List>
+using elem_v = typename elem<I, List>::value;
+
 template <typename List>
 struct reverse;
 
@@ -64,17 +85,44 @@ struct reverse<std::integer_sequence<Int, x>>
 template <typename Int, Int x, Int... xs>
 struct reverse<std::integer_sequence<Int, x, xs...>>
 {
-    using type = typename join<typename reverse<std::integer_sequence<Int, xs...>>::type,
-                               std::integer_sequence<Int, x>>::type;
+    using type = join_t<typename reverse<std::integer_sequence<Int, xs...>>::type,
+                               std::integer_sequence<Int, x>>;
 };
 
-template <std::size_t N, typename ValType, ValType Val, typename List>
+template <typename List>
+using reverse_t = typename reverse<List>::type;
+
+template <typename List, std::size_t N, auto Val>
 struct fill_val;
 
 template <std::size_t N, typename Int, Int Val, Int... Vals>
-struct fill_val<N, Int, Val, std::integer_sequence<Int, Vals...>>
+struct fill_val<std::integer_sequence<Int, Vals...>, N, Val>
 {
-    using type = typename join<std::integer_sequence<Int, Vals...>,
-                               typename repeat_integer<N, Int, Val>::type>::type;
+    using type = join_t<std::integer_sequence<Int, Vals...>,
+                               repeat_integers_t<N, Int, Val>>;
 };
+
+template <typename List, std::size_t N, auto Val>
+using fill_val_t = typename fill_val<List, N, Val>::type;
+
+template <typename List, auto Val>
+struct del;
+
+template <typename Int, Int Last, Int Val>
+struct del<std::integer_sequence<Int, Last>, Val>
+{
+    using type = typename std::conditional_t<Last == Val, std::integer_sequence<Int>, std::integer_sequence<int, Last>>;
+};
+
+template <typename Int, Int Val, Int Head, Int... Rest>
+struct del<std::integer_sequence<Int, Head, Rest...>, Val>
+{
+private:
+    using rest = std::integer_sequence<Int, Rest...>;
+public:
+    using type = typename std::conditional_t<Head == Val, rest, cons_int_t<Head, typename del<rest, Val>::type>>;
+};
+
+template <typename List, auto Val>
+using del_t = typename del<List, Val>::type;
 } // namespace lucid
